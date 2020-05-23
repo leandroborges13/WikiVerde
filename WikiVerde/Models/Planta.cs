048961614738a297417ae49b9b03d06a74baef4b
Leandro Souza Lima Borges <leandroborges87@yahoo.com.br>
2020-05-23 15:06:34 -0300

    Alteração da nomenclatura exibida para os campos.

--- WikiVerde/Models/Planta.cs
+++ WikiVerde/Models/Planta.cs
@@ -9,19 +9,22 @@ namespace WikiVerde.Models
     public class Planta
     {
         public Guid Id { get; set; }
-       /* [Required]
-        public bool IsPendente { get; set; }
-        public string Regiao { get; set; }
-        public double MaxPeso { get; set; }
-        public double MaxAltura { get; set; }
-        public double MinPeso { get; set; }
-        public double MinAltura { get; set; }
-        [Required]
-        public bool IsPerene { get; set; }
-        public int QtdRegasSemanais { get; set; }*/
-        [Required]
+        /* [Required]
+         public bool IsPendente { get; set; }
+         public string Regiao { get; set; }
+         public double MaxPeso { get; set; }
+         public double MaxAltura { get; set; }
+         public double MinPeso { get; set; }
+         public double MinAltura { get; set; }
+         [Required]
+         public bool IsPerene { get; set; }
+         public int QtdRegasSemanais { get; set; }*/
+        [Required(ErrorMessage = "O campo Nome cientifíco é obrigatório.")]
+        [Display(Name = "Nome cientifíco:")]
         public string NomeCientifico { get; set; }
+        [Display(Name = "Descrição da Espécie:")]
         public string DscEspecie { get; set; }
+        [Display(Name = "Descrição do manejo:")]
         public string DscManejo { get; set; }
         /*public string DscMuda { get; set; }
         public string DscReproducao { get; set; }
